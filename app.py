645d64cc1f6cbb0e62675c2c0947f1d91d686be5
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2018-01-23 22:17:36 -0500

    added base template, view function template and basic styles

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<myname>")
 def hello(myname='person'):
     return "Hello {0}".format(myname)
+    # return render_template('hello.html', name=myname)
+
 
 if __name__ == "__main__":
-    app.run(host=os.getenv("IP",'0.0.0.0'),
-    port=int(os.getenv("PORT", 8080)))
+    app.run(debug=True, host=os.getenv("IP", '0.0.0.0'), port=int(os.getenv("PORT", 8080)))
---
+++ static/app.js
@@ -0,0 +1,1 @@
+console.log('Hello, my JavaScript file has loaded');
